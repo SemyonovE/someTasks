edc88a52fb52e8f5e66084a775e4ea035230604e
Evgenij <semenov.evgeny.92@gmail.com>
2018-07-11 13:32:54 +0700

    Add 7-th task and change intro of the 6-th task

--- 6.js
+++ 6.js
@@ -9,12 +9,6 @@ function test(a, b, c) { return 'a=' + a + ',b=' + b + ',c=' + c; }
 var test1_3 = partialAny(test, 1, undefined, 3);
 console.log(test1_3(5)); // a=1,b=5,c=3
 
-console.log(test1_3()); // a=1,b=5,c=3
-
-function test(a, b, c) { return 'a=' + a + ',b=' + b + ',c=' + c; }
-var test1_3 = partialAny(test, 1, undefined, 3, undefined);
-console.log(test1_3(2)); // a=1,b=5,c=3
-
 function partialAny() {
     var fn = arguments[0];
     var argArray = argsToArray(arguments, 1);
